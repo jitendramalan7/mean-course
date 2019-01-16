8be326000e090e63f0ffced5b342fa0686f2e4ed
Jitendra Singh <singh.jitendra@tftus.com>
2019-01-16 18:00:03 +0530

    completed MEAN stack course

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
